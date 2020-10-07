df9069148b6e22b96b5a71c407dad45fda7a3466
AcherStyx <AcherStyx@gmail.com>
2020-10-07 19:06:20 +0800

    Reach end of schedule task

--- normal/base/utils.py
+++ normal/base/utils.py
@@ -22,38 +22,44 @@ class Expert:
     def __repr__(self):
         return self.__str__()
 
-    def assign(self,Task):
+    # 未完成对应的任务，但是需要将资源释放出来
+    def doRelease(self):
+        pass
+        # TODO: release method
+
+    def assign(self, task, timer):
         # check 专家负荷是否超载
-        if self.processing_task_num >= 3 :
+        if self.processing_task_num >= 3:
             raise FutureWarning
         # TODO:任务type与专家技能不匹配是否仍要分配,现默认任务匹配，专家将解决这一任务
-        if Task.task_type not in self.skill:
+        if task.task_type not in self.skill:
             pass
         # 专家负荷任务数+1,更新专家工作列表
         self.processing_task_num += 1
-        self.processing_task.append(Task)
+        self.processing_task.append(task)
 
         # 更新当前所分配任务Task的信息
-        Task.assigned_expert = self
+        task.assigned_expert = self
+        task.begin_time = timer
 
     # 默认调用该方法时，该任务已经完成
-    def finish(self,Task):
+    def finish(self, task):
         # 更新专家工作负荷量、工作列表、负荷任务数目
-        self.work_load += self.skill[Task.task_type]
-        self.processing_task.remove(Task)
+        self.work_load += self.skill[task.task_type]
+        self.processing_task.remove(task)
         self.processing_task_num -= 1
-
 
 
 class Task:
     def __init__(self,
                  task_id,
                  task_type,
-                 start_time,
+                 arrive_time,
                  time_limit):
         self.id = task_id
         self.task_type = task_type
-        self.start_time = start_time
+        self.arrive_time = arrive_time
+        self.begin_time = None
         self.time_limit = time_limit
         self.assigned_expert = None
         self.assigned_expert: Expert
@@ -61,7 +67,7 @@ class Task:
     def __str__(self):
         return "<id:{},type:{},start:{},limit:{}>".format(self.id,
                                                           self.task_type,
-                                                          self.start_time,
+                                                          self.arrive_time,
                                                           self.time_limit)
 
     def __repr__(self):
@@ -93,7 +99,7 @@ def load_task_list(work_order_file):
     for task in raw_task_list:
         task_list.append(Task(task_id=task[0],
                               task_type=task[2],
-                              start_time=task[1],
+                              arrive_time=task[1],
                               time_limit=task[3]))
     return task_list
 
@@ -101,6 +107,7 @@ def load_task_list(work_order_file):
 # 对于给定的任务，哪些专家可以完成这项任务
 def load_task_expert_dict(processing_matrix_file):
     data = expert_data(processing_matrix_file).sorted_expert_task_dict
+    return data
 
 
 if __name__ == '__main__':
@@ -109,12 +116,11 @@ if __name__ == '__main__':
     # test Expeet.assign and Expert.finish
     t = b[0]
     e = a[0]
-    print("专家",e)
-    print("任务",t)
+    print("专家", e)
+    print("任务", t)
     e.assign(t)
     print("分配任务后专家信息：{}".format(e))
-    print("分配任务后任务信息：{} assigned_expert:{}".format(t,t.assigned_expert))
+    print("分配任务后任务信息：{} assigned_expert:{}".format(t, t.assigned_expert))
     e.finish(t)
     print("任务完成后专家信息：{}".format(e))
     # print("任务完成后任务信息：{} assigned_expert:{}".format(t, t.assigned_expert))
-
--- normal/schedule/schedule_2.py
+++ normal/schedule/schedule_2.py
@@ -2,14 +2,16 @@ import logging
 from base.utils import *
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 if __name__ == '__main__':
     task_list = load_task_list("data/work_order.csv")
     expert_list = load_expert_list("data/process_time_matrix.csv")
+    task_expert_dict = load_task_expert_dict("data/process_time_matrix.csv")
 
     logger.debug(task_list)
     logger.debug(expert_list)
+    logger.debug(task_expert_dict)
 
     # 设置共计三个队列
     #   等待队列（未到达的任务队列）
@@ -24,19 +26,77 @@ if __name__ == '__main__':
     while True:
         timer += 1
 
-        logger.info("Current time: %s min", timer)
+        logger.warning(">>> Current time: %s min, in processing: %s, waiting: %s, not arrive: %s", timer,
+                       len(arrive_processing_list),
+                       len(arrive_wait_list),
+                       len(not_arrive_list))
+        logger.debug("All expert status: %s", expert_list)
 
         # 检查哪些任务完成了
+        logger.debug(arrive_processing_list)
+        remove_list = []
+
         for task in arrive_processing_list:
-            if task.start_time + task.assigned_expert.skill[task.task_type] == timer:
+            # print(task.task_type)
+            # print(task.assigned_expert)
+
+            logger.debug("Task ID: %s, Begin time: %s, Processing time: %s, Timer: %s",
+                         task.id,
+                         task.begin_time,
+                         task.assigned_expert.skill[task.task_type],
+                         timer)
+
+            if task.begin_time + task.assigned_expert.skill[task.task_type] == timer:
                 # 任务完成
-                arrive_processing_list.remove(task)
-            elif task.start_time + task.assigned_expert.skill[task.task_type] <= timer:
+                remove_list.append(task)  # 先放到一个删除列表中，之后再统一删除
+                task.assigned_expert.finish(task)
+                logger.info("[Finish] Task %s -> Expert %s", task.id, task.assigned_expert.id)
+            elif task.begin_time + task.assigned_expert.skill[task.task_type] <= timer:
                 # 明明应该早就完成的任务，还在列表中，错误
                 raise ValueError
+        for task in remove_list:
+            arrive_processing_list.remove(task)
 
         # 结束条件：全部队列空
         if not not_arrive_list and not arrive_wait_list and not arrive_processing_list:
             break
 
         # 队列调度
+
+        # 找到在此timer对应的时刻，有哪些任务到了
+        current_arrived_task_list = []
+        remove_list = []
+        for task in not_arrive_list:
+            if task.arrive_time == timer:
+                # 加入到新的临时列表，从未到达的列表中移出
+                current_arrived_task_list.append(task)
+                remove_list.append(task)
+        for task in remove_list:
+            not_arrive_list.remove(task)
+        current_arrived_task_list = arrive_wait_list + current_arrived_task_list
+        arrive_wait_list = []
+
+        # 尝试分配新到达的任务
+        for task in current_arrived_task_list:
+
+            # 看合适的专家中是否有空闲的，把他们列出来
+            suitable_expert_list = []
+            for suitable_expert in task_expert_dict[task.task_type]:
+                expert_id = suitable_expert[0]
+                # 专家可以再被分配任务
+                if expert_list[expert_id - 1].processing_task_num < 3:
+                    assert expert_list[expert_id - 1].id == expert_id
+                    suitable_expert_list.append(expert_list[expert_id - 1])
+
+            if not suitable_expert_list:  # 没有合适的专家，列表为空
+                arrive_wait_list.append(task)
+            else:  # 有合适的专家可以用来分配
+                arrive_processing_list.append(task)
+
+                # 从中选择一个专家，分配任务
+                suitable_expert_list.sort(key=lambda value: value.skill[task.task_type])
+                suitable_expert_list[0].assign(task, timer)
+                logger.info("[Assign] Task %s Type %s -> Expert %s",
+                            task.id,
+                            task.task_type,
+                            suitable_expert_list[0].id)
